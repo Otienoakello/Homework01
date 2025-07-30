8c89f3e07072e8cb567886284065036c7762aa21
Otienoakello <otienoakello23@gmail.com>
2025-07-30 01:21:04 -0600

    New beginning

---
+++ Desktop/ONE ON ONE/start.py
@@ -0,0 +1,14 @@
+import sys
+print("hello Silas")
+print(sys.version)
+if 10 > 12:
+    print("10 is greater than 12")
+else:
+    print("10 is not greater than 12")
+
+Name = "Silas"
+Sila = 2025
+Age = (2025 - 2001)
+print(Age)
+print("My Name is " + Name)
+print("My age is " + str(Age))
